af6af855a80a6895046c1b112d96417860da04bf
Joao Pedro <joaoaffonso@id.uff.br>
2023-06-10 06:50:28 -0300

    infojobs.com
    Adicionado a capacidade de scrapear o site do infojobs também.

--- vagas.py
+++ vagas.py
@@ -11,6 +11,7 @@ def carrossel(png_files, output_file):
     width, height = letter;
 
     for png_file in png_files:
+        #c.drawImage(png_file, 0, 0.2*height, width, 0.8*height)
         c.drawImage(png_file, 0, 0.2*height, width, 0.75*width)
         c.showPage()
 
@@ -58,6 +59,17 @@ def parse_page(url):
     else:
         print("Failed to retrieve the web page. Error code:", response.status_code)
 
+def parse_info_jobs(url):
+    response = requests.get(url);
+    soup = BeautifulSoup(response.content, 'html.parser')
+    meta_tag = soup.find('meta', attrs={'name': 'description'})
+    if meta_tag:
+        description = meta_tag.get('content')
+        return description
+    else:
+        return None
+
+
 def vagas_com(vetor):
     basic_url = "https://www.vagas.com.br/vagas-de-";
     total_urls = len(vetor);
@@ -102,6 +114,29 @@ def catho_com(vetor):
 
     return temp, dicionario
 
+def infojobs_com(vetor):
+    total_urls = len(vetor);
+    dicionario = {};
+    i = 0;
+    
+    for elemento in vetor:
+        x = parse_info_jobs(f"https://www.infojobs.com.br/empregos.aspx?palabra={elemento}")
+        
+        temp = x.split('Vagas de Emprego')[0];
+        
+        if temp != "":
+            dicionario[elemento] = int(temp);
+
+        i+=1;
+        print(f"Status: {format((100*i)/total_urls, '.2f')}%", end='\r')
+
+    temp = ""
+
+    for key, value in dicionario.items():
+        temp = temp + f"{datetime.now()}, {key}, {value}\n";
+
+    return temp, dicionario
+
 def write_file(name,text):
     file = open(name,"a");
     file.write(text);
@@ -114,7 +149,10 @@ def execute(to_print,vector,file_name,site_name,figura):
         temp, dicionario = catho_com(vector)
 
     if to_print.find("vagas") > 0:
-        temp, dicionario = vagas_com(vector)
+        temp, dicionario = vagas_com(vector);
+
+    if to_print.find("infojobs") > 0:
+        temp, dicionario = infojobs_com(vector);
 
     write_file(file_name,temp);
 
@@ -129,5 +167,7 @@ if __name__ == "__main__":
     execute("===  vagas.com: Linguagens   ===",langs,"vagas.csv","vagas.com","linguagens-vagas");
     execute("===  Catho.com: Certificados ===",certs,"catho.csv","catho.com","certificados-catho");
     execute("===  Catho.com: Linguagens   ===",langs,"catho.csv","catho.com","linguagens-catho");
+    execute("===  infojobs.com: Certificados ===",certs,"infojobs.csv","infojobs.com","certificados-infojobs");
+    execute("===  infojobs.com: Linguagens   ===",langs,"infojobs.csv","infojobs.com","linguagens-infojobs");
 
-    carrossel(['certificados-catho.png','certificados-vagas.png','linguagens-vagas.png','linguagens-catho.png'],"carrossel.pdf")
+    carrossel(['certificados-catho.png','certificados-vagas.png','certificados-infojobs.png','linguagens-vagas.png','linguagens-catho.png','linguagens-infojobs.png'],"carrossel.pdf")
